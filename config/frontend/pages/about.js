f804a58edafb08105b7b579eedb2023be56b8cb5
Richard Lovell <rjlovell@hotmail.com>
2022-09-26 21:28:43 +1300

    First commit

---
+++ config/frontend/pages/about.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import NewsletterBanner1 from "@/templates/banners/newsletter-banner-1/newsletter-banner-1";
+import Block2 from "@/templates/blocks/block-2/block-2";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+    <><Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <Block2 content={props.aboutEventsTeaserContent} />
+			 <NewsletterBanner1 content={props.newsletterContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'about'});
+  return {
+    props: props,
+    revalidate: 10
+  };
+}
---
+++ config/frontend/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import SEO from "@/templates/head/seo/seo";
+import NewsletterForm1 from "@/templates/forms/newsletter-form-1/newsletter-form-1";
+import Cards1 from "@/templates/cards/cards-1/cards-1";
+import HeroCover from "@/templates/covers/hero-cover/hero-cover";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+     <>
+	<SEO content={props.homeSeoContent} />
+		<Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <HeroCover content={props.eventsHeroContent} />
+			 <Cards1 content={props.eventsListContent} />
+			 <NewsletterForm1 content={props.newsletterLrgContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'home'});
+  return {
+    props: props,
+    revalidate: 10
+  };
+}
